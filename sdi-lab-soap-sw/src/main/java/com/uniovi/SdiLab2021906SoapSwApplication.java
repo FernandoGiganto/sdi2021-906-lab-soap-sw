d30d9acce9d10bad43ea22f27c922dbe5a2b0f02
FernandoGiganto <fernandogigantorodriguez@gmail.com>
2021-04-23 17:26:56 +0200

    SDI-2021-906-SW-11.1-Servicio SOAP

---
+++ sdi-lab-soap-sw/src/main/java/com/uniovi/SdiLab2021906SoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiLab2021906SoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiLab2021906SoapSwApplication.class, args);
+	}
+
+}
